d11b998d1c512619f320b39c7a398f019b4b7129
CjArpit18 <arpit20@lilly.com>
2025-09-04 13:26:50 +0530

    Create sample_notebook.py

---
+++ src/sample_notebook.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md
+# # Sample Notebook
+# This is a demo Databricks notebook for DAB deployment.
+
+# COMMAND ----------
+print("Hello from Databricks Asset Bundles notebook!")
