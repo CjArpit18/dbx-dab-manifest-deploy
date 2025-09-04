cd7acf9ff4be7b0a8184c1b4a3606f99f847a98d
CjArpit18 <arpit20@lilly.com>
2025-09-04 18:10:04 +0530

    Update experiment.py

--- src/experiment.py
+++ src/experiment.py
@@ -1,1 +1,7 @@
-print("This is still in development")
+# Databricks notebook source
+# MAGIC %md
+# # Experimental Notebook
+# This is a demo Databricks notebook for DAB deployment.
+
+# COMMAND ----------
+print("Hello from Databricks Asset Bundles notebook! V3")
